6a2944a6ebf242e0bf8ae65afa7c21e89e8af568
scy <tjcksdud0221@naver.com>
2025-06-20 00:12:54 +0900

    [feat] 예외 반환 양식 변경
    ErrorCode에 포함된 HttpStatus가 반환되도록 했습니다.
    또한 식별코드가 추가되어 에러파악을 용이하게 했습니다.
    
    관련 이슈: #3

--- backend/src/main/java/com/example/backend/exception/ErrorInfo.java
+++ backend/src/main/java/com/example/backend/exception/ErrorInfo.java
@@ -11,6 +11,7 @@ public class ErrorInfo {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
     private Instant timestamp;
     private int status;
+    private String code;
     private String message;
     private String path;
 }
