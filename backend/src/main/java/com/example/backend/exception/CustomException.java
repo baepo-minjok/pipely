900aa36ff0ddb4a101bb725becb42e95ec13d878
scy <tjcksdud0221@naver.com>
2025-06-19 18:20:27 +0900

    [feat] 전역 예외 처리
    전역 예외 처리하는 GlobalExceptionHandler 만들었구요.
    ErrorCode에 등록하고
    사용자 설정 예외 타입인 CustomException를 이용해서
    예외처리를 해주면 됩니다.
    
    관련 이슈: #3

---
+++ backend/src/main/java/com/example/backend/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.example.backend.exception;
+
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return this.errorCode;
+    }
+}
