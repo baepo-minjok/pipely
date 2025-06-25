b6d4ae53c038f881e9c2f82be7745ba3c09b859f
seiyeon <wsx4379@naver.com>
2025-06-25 16:55:28 +0900

    [feat] 비밀번호 변경 페이지 구현
    비밀번호 변경 페이지 구현

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from 'vue-router';
 import HelloWorld from '../components/HelloWorld.vue';
 import Login from '../pages/users/Login.vue';
 import Signup from '../pages/users/Signup.vue';
+import FindPassword from '../pages/users/FindPassword.vue';
 
 const routes = [
   { path: '/', component: HelloWorld },
   { path: '/login', component: Login },
   { path: '/signup', component: Signup },
+  { path: '/find/password', component: FindPassword },
 ];
 
 const router = createRouter({
