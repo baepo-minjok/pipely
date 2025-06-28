fafed1a60ad4dd32dde2e15820b8440b56d2c6c1
scy <tjcksdud0221@naver.com>
2025-06-28 18:26:41 +0900

    [feat] FreeStyle job 수정
    FreeStyle job을 수정하는 엔드포인트를 추가했습니다.
    서버에서 설정한 내용은 변하지 않고 프로젝트에서 관리하는 부분만 변경하도록 설정했습니다.
    
    관련 이슈: #36

--- backend/src/main/java/com/example/backend/jenkins/job/model/dto/JobRequestDto.java
+++ backend/src/main/java/com/example/backend/jenkins/job/model/dto/JobRequestDto.java
@@ -7,10 +7,25 @@ import java.util.UUID;
 public class JobRequestDto {
 
     @Getter
-    public static class CreateFreestyleDto {
+    public static class CreateFreeStyleDto {
 
         private UUID infoId;
 
+        private String jobName;
+        private String description;
+        private String projectUrl;
+        private String projectDisplayName;
+        private Boolean githubTrigger;
+        private String repositoryUrl;
+        private String branch;
+        private String script;
+    }
+
+    @Getter
+    public static class UpdateFreeStyleDto {
+
+        private UUID infoId;
+        private UUID freeStyleId;
         private String jobName;
         private String description;
         private String projectUrl;
