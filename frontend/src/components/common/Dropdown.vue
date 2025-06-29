8056055a878e3d153c41513aa178b0c3434602b1
seiyeon <wsx4379@naver.com>
2025-06-29 20:24:02 +0900

    [feat] 드롭다운 메뉴 구현
    드롭다운 메뉴 구현

--- frontend/src/components/common/Dropdown.vue
+++ frontend/src/components/common/Dropdown.vue
@@ -3,29 +3,43 @@
 </script>
 
 <template>
-  <div class="container">
+  <div class="dropdown_container">
     <ul class="list">
-      <li>파이프라인 목록</li>
-      <li>마이페이지</li>
-      <li>로그아웃</li>
+      <li><router-link to="/" class="list_item">파이프라인 목록</router-link></li>
+      <li><router-link to="/" class="list_item">마이페이지</router-link></li>
+      <li><router-link to="/" class="list_item">로그아웃</router-link></li>
     </ul>
   </div>
 </template>
 
 <style scoped>
-.container {
+.dropdown_container {
   position: absolute;
-  top: 0;
+  top: 100%;
   right: 0;
-  width: fit-content;
+  z-index: 100;
+  min-width: 110px;
   background-color: white;
   border-radius: 10px;
   box-shadow: 2px 2px 4px 2px var(--gray400);
   padding: 21px 24px;
 }
 
-.list > li {
+.list {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+
+.list_item{
   list-style: none;
+  cursor: pointer;
+  text-decoration: none;
+  color: var(--gray900);
+
+  &:hover {
+    font-weight: 700;
+  }
 }
 
 </style>
--- frontend/src/components/common/Header.vue
+++ frontend/src/components/common/Header.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import Dropdown from './Dropdown.vue';
 
@@ -14,13 +14,33 @@ const handleMenuClick  = () => {
   showMenu.value = !showMenu.value;
 }
 
+// 드롭다운 영역을 참조할 ref
+const profileWrapper = ref(null);
+
+// 바깥 클릭 감지 함수
+const handleClickOutside = (event) => {
+  if (profileWrapper.value && !profileWrapper.value.contains(event.target)) {
+    showMenu.value = false;
+  }
+};
+
+onMounted(() => {
+  document.addEventListener('click', handleClickOutside);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', handleClickOutside);
+});
+
 </script>
 
 <template>
   <div class="container">
     <img src="/src/assets/images/logo.png" alt="pipely" class="logo_img" @click="goToHome()">
-    <img src="/src/assets/icons/profile.svg" alt="profile" class="profile_icon" @click="handleMenuClick()">
-    <Dropdown v-if="showMenu" />
+    <div class="profile_wrapper" ref="profileWrapper">
+      <img src="/src/assets/icons/profile.svg" alt="profile" class="profile_icon" @click="handleMenuClick()">
+      <Dropdown v-if="showMenu" />
+    </div>
   </div>
 </template>
 
@@ -49,5 +69,9 @@ const handleMenuClick  = () => {
   width: 30px;
   height: 30px;
   cursor: pointer;
+}
+
+.profile_wrapper {
+  position: relative;
 }
 </style>
