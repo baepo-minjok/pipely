e2907dde1149523cf36f4fbef8ef3f8f70757e5a
seiyeon <wsx4379@naver.com>
2025-06-29 16:02:54 +0900

    [feat] 드롭다운 메뉴 구현 중
    드롭다운 메뉴 구현 중

---
+++ frontend/src/components/common/Dropdown.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <ul class="list">
+      <li>파이프라인 목록</li>
+      <li>마이페이지</li>
+      <li>로그아웃</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: fit-content;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 2px 2px 4px 2px var(--gray400);
+  padding: 21px 24px;
+}
+
+.list > li {
+  list-style: none;
+}
+
+</style>
