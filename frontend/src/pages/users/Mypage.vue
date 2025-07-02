0a61f3cf9c28c4cb612c31e676ab5c898700298e
seiyeon <wsx4379@naver.com>
2025-07-02 15:29:19 +0900

    [feat] 마이페이지 cicd 정보 부분 구현
    마이페이지 cicd 정보 부분 구현

--- frontend/src/pages/users/Mypage.vue
+++ frontend/src/pages/users/Mypage.vue
@@ -1,7 +1,30 @@
 <script setup>
 import Header from '../../components/common/Header.vue';
+import { ref } from 'vue';
 
+const openDropdown = ref(false);
 
+const cicdItems = ref([
+  {
+    label: 'Jenkins',
+    image: '/src/assets/images/jenkins.png',
+  },
+  {
+    label: 'GitLab',
+    image: 'https://about.gitlab.com/images/press/logo/png/gitlab-icon-rgb.png',
+  },
+]);
+
+const selectedItem = ref(cicdItems.value[0]);
+
+function toggleDropdown() {
+  openDropdown.value = !openDropdown.value;
+}
+
+function selectItem(item) {
+  selectedItem.value = item;
+  openDropdown.value = false;
+}
 </script>
 
 <template>
@@ -16,24 +39,60 @@ import Header from '../../components/common/Header.vue';
 
             <div class="email_text">
               <p>test01@example.com</p>
-              <img src="/src/assets/icons/check.svg" alt="check_icon">
+              <img src="/src/assets/icons/check.svg" alt="check_icon" />
             </div>
             <button class="info_btn">인증 메일 발송</button>
-
           </div>
 
           <div class="item_box">
             <p class="item_label">이름</p>
             <p>test01</p>
-          </div> 
+          </div>
           <div class="item_box">
             <p class="item_label">비밀번호</p>
             <p>••••••••••••</p>
             <button class="info_btn">비밀번호 변경</button>
-          </div> 
+          </div>
         </div>
       </div>
-      <div class="cicd_box"></div>
+      <div class="cicd_wrapper">
+        <h3 class="title">CI/CD 정보</h3>
+        <div class="cicd_header">
+          <div class="dropdown_container">
+            <button @click="toggleDropdown" class="dropdown">
+              <div>
+                <img :src="selectedItem.image" alt="icon" class="dropdown_img" />
+                <span class="dropdown_label">{{ selectedItem.label }}</span>
+              </div>
+              <img src="/src/assets/icons/down_arrow.svg" alt="down_icon" class="dropdown_arrow" />
+            </button>
+
+            <div v-if="openDropdown" class="dropdown_menu">
+              <ul>
+                <li v-for="(item, index) in cicdItems" :key="index" @click="selectItem(item)" class="dropdown_item">
+                  <img :src="item.image" alt="icon" class="dropdown_img" />
+                  <span>{{ item.label }}</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img src="/src/assets/icons/plus_circle.svg" alt="plus_btn" class="plus_btn" />
+        </div>
+        <div class="cicd_card_list">
+          <div class="cicd_card">
+            <p>젠킨스 01</p>
+            <p>https://jenkins.io/abcdefghehe</p>
+          </div>
+          <div class="cicd_card">
+            <p>젠킨스 01</p>
+            <p>https://jenkins.io/abcdefghehe</p>
+          </div>
+          <div class="cicd_card">
+            <p>젠킨스 01</p>
+            <p>https://jenkins.io/abcdefghehe</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -41,7 +100,6 @@ import Header from '../../components/common/Header.vue';
 <style scoped>
 .container {
   width: 100%;
-  height: 100%;
 }
 
 .wrapper {
@@ -52,9 +110,11 @@ import Header from '../../components/common/Header.vue';
 .title {
   font-size: 22px;
   font-weight: 500;
-  color: #4A5464;
+  color: #4a5464;
   margin-bottom: 18px;
 }
+
+/* 기본 정보 */
 
 .info_box {
   background-color: white;
@@ -72,20 +132,18 @@ import Header from '../../components/common/Header.vue';
   align-items: center;
   padding: 0 10px;
   box-sizing: border-box;
-
 }
 
 .item_box:not(:last-of-type) {
   border-bottom: 1px solid var(--gray200);
   padding-bottom: 22px; /* 아래 여백 */
-  margin-bottom: 22px;  /* 선과 다음 요소 사이 여백 */
+  margin-bottom: 22px; /* 선과 다음 요소 사이 여백 */
 }
 
 .item_label {
   color: var(--gray500);
   white-space: nowrap;
 }
-
 
 .email_text {
   display: flex;
@@ -105,4 +163,120 @@ import Header from '../../components/common/Header.vue';
   }
 }
 
+/* ci/cd 정보 */
+.cicd_wrapper {
+  margin-top: 76px;
+}
+
+.cicd_header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+/* 드롭다운 */
+.dropdown_container {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown {
+  width: 180px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: white;
+  border: 1px solid var(--gray200);
+  padding: 8px 12px;
+  cursor: pointer;
+  font-size: 16px;
+
+  & > div {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.dropdown_img {
+  width: 20px;
+  height: 20px;
+  margin-right: 6px;
+}
+
+.dropdown_label {
+  white-space: nowrap;
+}
+
+.dropdown_arrow {
+  width: 16px;
+  height: 16px;
+}
+
+.dropdown_menu {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  min-width: 100%;
+  background: white;
+  border: 1px solid var(--gray200);
+  margin-top: 2px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+  z-index: 10;
+  box-sizing: border-box;
+}
+
+.dropdown_item {
+  display: flex;
+  align-items: center;
+  padding: 8px 12px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--gray100);
+  }
+}
+
+.dropdown_item:hover {
+  background: #f0f0f0;
+}
+
+.plus_btn {
+  width: 26px;
+  cursor: pointer;
+}
+
+.cicd_card_list {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 19px;
+  padding: 28px;
+  background-color: var(--gray100);
+  margin-top: 10px;
+}
+
+.cicd_card {
+  border-radius: 12px;
+  border: 1px solid var(--gray200);
+  background-color: white;
+  padding: 32px;
+  word-break: break-all;
+  cursor: pointer;
+  transition: scale 0.3s;
+
+  & > p:last-child {
+    margin-top: 17px;
+    color: var(--gray500);
+    font-size: 14px;
+  }
+
+  &:hover {
+    scale: 1.05;
+  }
+}
+
+.cicd_card > p:last-child {
+  margin-top: 17px;
+  color: var(--gray500);
+  font-size: 14px;
+}
 </style>
