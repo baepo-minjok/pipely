ad505953627ebc1e13926db432a583db0dc4ef4a
Taehoon <cjsxogns0518@gmail.com>
2025-07-03 17:39:36 +0900

    [refactor] job_notification 엔티티 구조 변경

--- backend/src/main/java/com/example/backend/jenkins/job/model/JobNotification.java
+++ backend/src/main/java/com/example/backend/jenkins/job/model/JobNotification.java
@@ -1,7 +1,7 @@
 package com.example.backend.jenkins.job.model;
 
 import jakarta.persistence.*;
-
+import java.util.UUID;
 import java.time.LocalDateTime;
 
 @Entity
@@ -10,14 +10,18 @@ import java.time.LocalDateTime;
 public class JobNotification {
 
     @Id
-    @Column(name = "name")
-    private String name;
-
+    @Column(name = "id", nullable = false)
+    private UUID id;
 
     @Id
-    @Column(name = "job_created_at")
-    private LocalDateTime jobCreatedAt;
+    @Column(name = "name", nullable = false)
+    private String name;
 
+    @Id
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "should_notify")
     private Boolean shouldNotify;
 
     private String channel;
--- backend/src/main/java/com/example/backend/jenkins/job/model/JobNotificationId.java
+++ backend/src/main/java/com/example/backend/jenkins/job/model/JobNotificationId.java
@@ -2,8 +2,10 @@ package com.example.backend.jenkins.job.model;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class JobNotificationId implements Serializable {
+    private UUID id;
     private String name;
     private LocalDateTime jobCreatedAt;
 }
