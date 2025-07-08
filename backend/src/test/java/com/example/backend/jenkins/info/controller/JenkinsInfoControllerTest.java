a14cfe89a070d76ca63166e39a71e79d5af4adc5
scy <tjcksdud0221@naver.com>
2025-07-08 18:31:27 +0900

    [feat] JenkinsInfoController 테스트 코드 추가
    JenkinsInfoController의 각 엔드포인트 실페시 예외 테스트 코드 추가했습니다.
    
    관련 이슈: #87

--- backend/src/test/java/com/example/backend/jenkins/info/controller/JenkinsInfoControllerTest.java
+++ backend/src/test/java/com/example/backend/jenkins/info/controller/JenkinsInfoControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.backend.jenkins.info.controller;
 
 import com.example.backend.config.jwt.JwtTokenProvider;
+import com.example.backend.exception.CustomException;
+import com.example.backend.exception.ErrorCode;
 import com.example.backend.jenkins.info.model.dto.InfoRequestDto.CreateDto;
 import com.example.backend.jenkins.info.model.dto.InfoRequestDto.InfoDto;
 import com.example.backend.jenkins.info.model.dto.InfoRequestDto.UpdateDto;
@@ -9,20 +11,24 @@ import com.example.backend.jenkins.info.service.JenkinsInfoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,6 +48,42 @@ public class JenkinsInfoControllerTest {
 
     @MockitoBean
     private JwtTokenProvider tokenProvider;
+
+    static Stream<Arguments> verificationExceptionProvider() {
+        return Stream.of(
+                Arguments.of(
+                        ErrorCode.JENKINS_AUTHENTICATION_FAILED,
+                        HttpStatus.NOT_FOUND,
+                        "JENKINS_AUTHENTICATION_FAILED_404"
+                ),
+                Arguments.of(
+                        ErrorCode.JENKINS_ENDPOINT_NOT_FOUND,
+                        HttpStatus.NOT_FOUND,
+                        "JENKINS_ENDPOINT_NOT_FOUND_404"
+                ),
+                Arguments.of(
+                        ErrorCode.JENKINS_URI_NOT_FOUND,
+                        HttpStatus.NOT_FOUND,
+                        "JENKINS_URI_NOT_FOUND_404"
+                ),
+                Arguments.of(
+                        ErrorCode.JENKINS_CONNECTION_FAILED,
+                        HttpStatus.NOT_FOUND,
+                        "JENKINS_CONNECTION_FAILED_404"
+                ),
+                Arguments.of(
+                        ErrorCode.JENKINS_SERVER_ERROR,
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        "JENKINS_SERVER_ERROR_500"
+                ),
+                Arguments.of(
+                        ErrorCode.JENKINS_CONNECTION_TIMEOUT_OR_NETWORK_ERROR,
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        "JENKINS_CONNECTION_TIMEOUT_OR_NETWORK_ERROR_500"
+                )
+
+        );
+    }
 
     @Test
     @DisplayName("POST /api/jenkins/info/create - 성공 시 200 반환 및 서비스 호출")
@@ -67,6 +109,26 @@ public class JenkinsInfoControllerTest {
     }
 
     @Test
+    @DisplayName("POST /api/jenkins/info/create - 필수값 누락시 400 반환")
+    void createInfo_valid() throws Exception {
+        CreateDto dto = CreateDto.builder()
+                .name("my jenkins")
+                .description("회사 ci 서버입니다.")
+                .jenkinsId("admin")
+                .apiToken("33333")
+                .build();
+
+        mockMvc.perform(post("/api/jenkins/info/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("VALIDATION_FAILED_400"))
+                .andExpect(jsonPath("$.error.message").exists());
+    }
+
+    @Test
     @DisplayName("PUT /api/jenkins/info - 성공 시 200 반환 및 서비스 호출")
     void updateInfo_success() throws Exception {
         UpdateDto dto = UpdateDto.builder()
@@ -82,11 +144,54 @@ public class JenkinsInfoControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data").value("Jenkins Info update Success"))
                 .andExpect(jsonPath("$.error").doesNotExist());
 
         verify(jenkinsInfoService).updateJenkinsInfo(eq(dto));
+    }
+
+    @Test
+    @DisplayName("PUT /api/jenkins/info - 없는 jenkins info를 수정하면 404 반환")
+    void updateInfo_not_found() throws Exception {
+        UpdateDto dto = UpdateDto.builder()
+                .infoId(UUID.randomUUID())
+                .uri("http://jenkins.example.com")
+                .name("not my jenkins")
+                .description("회사 ci 서버 아닙니다.")
+                .jenkinsId("admin")
+                .apiToken("token1243")
+                .build();
+
+        when(jenkinsInfoService.updateJenkinsInfo(eq(dto)))
+                .thenThrow(new CustomException(ErrorCode.JENKINS_INFO_NOT_FOUND));
+
+        mockMvc.perform(put("/api/jenkins/info")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("JENKINS_INFO_NOT_FOUND_404"))
+                .andExpect(jsonPath("$.error.message").exists());
+    }
+
+    @Test
+    @DisplayName("PUT /api/jenkins/info - 필수값 누락시 400 반환")
+    void updateInfo_valid() throws Exception {
+        UpdateDto dto = UpdateDto.builder()
+                .infoId(UUID.randomUUID())
+                .name("not my jenkins")
+                .description("회사 ci 서버 아닙니다.")
+                .jenkinsId("admin")
+                .apiToken("token1243")
+                .build();
+
+        mockMvc.perform(put("/api/jenkins/info")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("VALIDATION_FAILED_400"))
+                .andExpect(jsonPath("$.error.message").exists());
     }
 
     @Test
@@ -134,6 +239,40 @@ public class JenkinsInfoControllerTest {
     }
 
     @Test
+    @DisplayName("POST /api/jenkins/info - 없는 ID로 상세 조회 시 404 반환")
+    void getDetailInfo_notFound() throws Exception {
+        UUID id = UUID.randomUUID();
+        InfoDto dto = InfoDto.builder().infoId(id).build();
+
+        when(jenkinsInfoService.getDetailInfoById(id))
+                .thenThrow(new CustomException(ErrorCode.JENKINS_INFO_NOT_FOUND));
+
+        mockMvc.perform(post("/api/jenkins/info")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("JENKINS_INFO_NOT_FOUND_404"))
+                .andExpect(jsonPath("$.error.message").exists());
+
+        verify(jenkinsInfoService).getDetailInfoById(id);
+    }
+
+    @Test
+    @DisplayName("POST /api/jenkins/info - 상세 조회 시 ID 누락으로 400 반환")
+    void getDetailInfo_validationFail() throws Exception {
+        InfoDto dto = InfoDto.builder().build();
+
+        mockMvc.perform(post("/api/jenkins/info")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("VALIDATION_FAILED_400"))
+                .andExpect(jsonPath("$.error.message").exists());
+    }
+
+    @Test
     @DisplayName("DELETE /api/jenkins/info/{infoId} - 삭제 시 200 반환 및 서비스 호출")
     void deleteInfo_success() throws Exception {
         UUID id = UUID.randomUUID();
@@ -142,6 +281,23 @@ public class JenkinsInfoControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data").value("delete Jenkins Info Success"))
                 .andExpect(jsonPath("$.error").doesNotExist());
+
+        verify(jenkinsInfoService).deleteJenkinsInfo(id);
+    }
+
+    @Test
+    @DisplayName("DELETE /api/jenkins/info/{infoId} - 없는 ID 삭제 시 404 반환")
+    void deleteInfo_notFound() throws Exception {
+        UUID id = UUID.randomUUID();
+
+        doThrow(new CustomException(ErrorCode.JENKINS_INFO_NOT_FOUND))
+                .when(jenkinsInfoService).deleteJenkinsInfo(id);
+
+        mockMvc.perform(delete("/api/jenkins/info/{infoId}", id))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("JENKINS_INFO_NOT_FOUND_404"))
+                .andExpect(jsonPath("$.error.message").exists());
 
         verify(jenkinsInfoService).deleteJenkinsInfo(id);
     }
@@ -158,6 +314,51 @@ public class JenkinsInfoControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data").value("verify success"))
                 .andExpect(jsonPath("$.error").doesNotExist());
+
+        verify(jenkinsInfoService).verificationJenkinsInfo(id);
+    }
+
+    @Test
+    @DisplayName("POST /api/jenkins/info/verification - 검증 실패 시 404 반환")
+    void verifyInfo_notFound() throws Exception {
+        UUID id = UUID.randomUUID();
+        InfoDto dto = InfoDto.builder().infoId(id).build();
+
+        doThrow(new CustomException(ErrorCode.JENKINS_INFO_NOT_FOUND))
+                .when(jenkinsInfoService).verificationJenkinsInfo(id);
+
+        mockMvc.perform(post("/api/jenkins/info/verification")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("JENKINS_INFO_NOT_FOUND_404"))
+                .andExpect(jsonPath("$.error.message").exists());
+
+        verify(jenkinsInfoService).verificationJenkinsInfo(id);
+    }
+
+    @ParameterizedTest(name = "[{index}] {0} 예외일 때 {1} 반환, code={2}")
+    @MethodSource("verificationExceptionProvider")
+    void verifyInfo_errorResponses(
+            ErrorCode errorCode,
+            HttpStatus expectedStatus,
+            String expectedJsonCode
+    ) throws Exception {
+
+        UUID id = UUID.randomUUID();
+        InfoDto dto = InfoDto.builder().infoId(id).build();
+        doThrow(new CustomException(errorCode))
+                .when(jenkinsInfoService)
+                .verificationJenkinsInfo(id);
+
+        mockMvc.perform(post("/api/jenkins/info/verification")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().is(expectedStatus.value()))
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value(expectedJsonCode))
+                .andExpect(jsonPath("$.error.message").exists());
 
         verify(jenkinsInfoService).verificationJenkinsInfo(id);
     }
