28ee6b22109ae579760b18f4ce9b618277d8c176
scy <tjcksdud0221@naver.com>
2025-07-09 19:51:52 +0900

    [feat] RequestDto 수정
    요청 받는 내용과 어노테이션을 추가했습니다.
    
    관련 이슈: #98

--- backend/src/main/java/com/example/backend/jenkins/job/model/dto/RequestDto.java
+++ backend/src/main/java/com/example/backend/jenkins/job/model/dto/RequestDto.java
@@ -1,8 +1,22 @@
 package com.example.backend.jenkins.job.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
 public class RequestDto {
 
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class CreateDto {
+
+        // jenkins 정보 id
+        private UUID infoId;
 
         // job 이름
         private String name;
@@ -12,6 +26,12 @@ public class RequestDto {
 
         // 연동된 git 주소
         private String githubUrl;
+
+        // clone할 branch 이름
+        private String branch;
+
+        // 빌드를 실행할 폴더 경로
+        private String directory;
 
         // git webhook trigger 설정 여부
         private Boolean trigger;
