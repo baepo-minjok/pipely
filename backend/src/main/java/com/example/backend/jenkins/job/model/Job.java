4df0fcd1af318640321d4f0f6bb27ba2833eb0d2
scy <tjcksdud0221@naver.com>
2025-07-09 20:22:36 +0900

    [feat] create Job 엔드포인트 생성
    job을 생성하는 엔드포인트를 만들고 e2e테스트도 진행했습니다.
    
    관련 이슈: #98

--- backend/src/main/java/com/example/backend/jenkins/job/model/Job.java
+++ backend/src/main/java/com/example/backend/jenkins/job/model/Job.java
@@ -1,12 +1,12 @@
 package com.example.backend.jenkins.job.model;
 
+import com.example.backend.jenkins.info.model.JenkinsInfo;
 import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
 
 @Entity
-@Table(name = "job")
 public class Job {
 
     @Id
@@ -24,6 +24,12 @@ public class Job {
     // 연동된 git 주소
     private String githubUrl;
 
+    // clone할 branch 이름
+    private String branch;
+
+    // 빌드를 실행할 폴더 경로
+    private String directory;
+
     // git webhook trigger 설정 여부
     private Boolean trigger;
 
@@ -32,4 +38,8 @@ public class Job {
 
     // 선택한 Build 도구
     private String buildTool;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "jenkins_info_id", nullable = false)
+    private JenkinsInfo jenkinsInfo;
 }
