f8f6e2547e711ee1deec3571db067ef06cad839a
= <sjhyun0926@gmail.com>
2025-07-18 17:19:03 +0900

    [FIX] 테스트수정 - jpaAuditing수정

--- backend/src/test/java/com/example/backend/TestJpaAuditingConfig.java
+++ backend/src/test/java/com/example/backend/TestJpaAuditingConfig.java
@@ -3,10 +3,12 @@ package com.example.backend;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Optional;
 
 @Configuration
+@EnableJpaAuditing
 public class TestJpaAuditingConfig {
 
     @Bean
