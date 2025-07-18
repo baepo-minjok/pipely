1d39f57863c6ff409405199594669b469e29843b
= <sjhyun0926@gmail.com>
2025-07-18 23:12:44 +0900

    [FIX] 테스트수정2 - jpaAuditing수정

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(TestJpaAuditingConfig.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class BackendApplicationTests {
