dcda21f315c4f3ca963660c3238585bfdd553b8a
leewoojin <zxsa999o@gmail.com>
2025-07-21 20:58:09 +0900

    주석 에러 삭제

--- frontend/src/components/jobs/JobCard.vue
+++ frontend/src/components/jobs/JobCard.vue
@@ -12,28 +12,6 @@ const props = defineProps({
 })
 const { job } = toRefs(props)
 
-// const job = ({
-//   idx: 1,
-//   name: 'CI/CD Demo 01',
-//   createdBy: '이우진',
-//   lastExe: '2025-06-11T14:33:00',
-//   stages: [
-//     {
-//       type: 'Build',
-//       state: 'SUCCESS',
-//     },
-//     {
-//       type: 'Test',
-//       state: 'SUCCESS',
-//     },
-//     {
-//       type: 'Deploy',
-//       state: 'SUCCESS',
-//     },
-//   ],
-//   buildState: 'SUCCESS',
-// });
-
 
 
 </script>
