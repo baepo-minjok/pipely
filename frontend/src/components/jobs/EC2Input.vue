d37218e58cc21b2f61ddf643faa3278bf0e0dece
seiyeon <wsx4379@naver.com>
2025-07-21 17:55:48 +0900

    [feat] 배포 input 컴포넌트 구현
    배포 input 컴포넌트 구현

---
+++ frontend/src/components/jobs/EC2Input.vue
@@ -0,0 +1,38 @@
+<script setup>
+const props = defineProps({
+  form: Object,
+});
+</script>
+
+<template>
+  <div class="input_group">
+    <label>EC2 JAR 경로</label>
+    <input v-model="form.ec2DeployPath" placeholder="예: /home/ec2-user/app" />
+  </div>
+</template>
+
+<style scoped>
+.input_group {
+  display: flex;
+  flex-direction: column;
+  gap: 11px;
+}
+
+label {
+  color: var(--gray800);
+}
+
+input {
+  width: 100%;
+  background-color: white;
+  border-radius: 8px;
+  border: 1px solid var(--gray200);
+  padding: 13px 16px;
+  box-sizing: border-box;
+  font-size: 16px;
+
+  &::placeholder {
+    color: var(--gray400);
+  }
+}
+</style>
