324fbaa6d1e2fbf802e0255fcae28a232414d5ae
seiyeon <wsx4379@naver.com>
2025-07-25 02:52:24 +0900

    [chore] JobList name 추가
    JobList name 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
   { path: '/mypage', component: Mypage, name: 'Mypage', meta: { requiresAuth: true } },
   { path: '/mypage/cicd/create', component: CreateCicdInfo, meta: { requiresAuth: true } },
   { path: '/mypage/cicd/:id', component: CicdInfoDetail, meta: { requiresAuth: true } },
-  { path: '/job', component: JobList, meta: { requiresAuth: true } },
+  { path: '/job', component: JobList, name: 'JobList', meta: { requiresAuth: true } },
   { path: '/job/create', component: CreateJob, name: 'CreateJob', meta: { requiresAuth: true } },
   { path: '/chat', component: Chat },
   { path: '/:catchAll(.*)', redirect: '/' },
