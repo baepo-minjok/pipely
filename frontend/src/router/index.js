a5b10518f6f6d56a2583e0537ac55fbb9c0ff047
seiyeon <wsx4379@naver.com>
2025-07-24 13:48:55 +0900

    [feat] CreateJob name 추가
    CreateJob name 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
-import {createRouter, createWebHistory} from 'vue-router';
-import {useUserStore} from "@/stores/useUserStore.js"
-import {userApi} from "@/api/UserApi.js";
+import { createRouter, createWebHistory } from 'vue-router';
+import { useUserStore } from '@/stores/useUserStore.js';
+import { userApi } from '@/api/UserApi.js';
 import Login from '../pages/users/Login.vue';
 import Signup from '../pages/users/Signup.vue';
 import FindPassword from '../pages/users/FindPassword.vue';
@@ -14,18 +14,18 @@ import OAuth from '../pages/users/OAuthSignup.vue';
 import VerifyEmail from '../pages/users/VerifyEmail.vue';
 
 const routes = [
-    {path: '/', component: Main, name: 'Main'},
-    {path: '/user/login', component: Login, name: 'Login'},
-    {path: '/user/oAuth', component: OAuth, name: 'OAuth'},
-    {path: '/user/signup', component: Signup, name: 'Signup'},
-    {path: '/user/email/verify', component: VerifyEmail},
-    {path: '/user/find/password', component: FindPassword, meta: {requiresAuth: true}},
-    {path: '/mypage', component: Mypage, name: 'Mypage', meta: {requiresAuth: true}},
-    {path: '/mypage/cicd/create', component: CreateCicdInfo, meta: {requiresAuth: true}},
-    {path: '/mypage/cicd/:id', component: CicdInfoDetail, meta: {requiresAuth: true}},
-    {path: '/job', component: JobList, meta: {requiresAuth: true}},
-    {path: '/job/create', component: CreateJob, meta: {requiresAuth: true}},
-    {path: '/:catchAll(.*)', redirect: '/'},
+  { path: '/', component: Main, name: 'Main' },
+  { path: '/user/login', component: Login, name: 'Login' },
+  { path: '/user/oAuth', component: OAuth, name: 'OAuth' },
+  { path: '/user/signup', component: Signup, name: 'Signup' },
+  { path: '/user/email/verify', component: VerifyEmail },
+  { path: '/user/find/password', component: FindPassword, meta: { requiresAuth: true } },
+  { path: '/mypage', component: Mypage, name: 'Mypage', meta: { requiresAuth: true } },
+  { path: '/mypage/cicd/create', component: CreateCicdInfo, meta: { requiresAuth: true } },
+  { path: '/mypage/cicd/:id', component: CicdInfoDetail, meta: { requiresAuth: true } },
+  { path: '/job', component: JobList, meta: { requiresAuth: true } },
+  { path: '/job/create', component: CreateJob, name: 'CreateJob', meta: { requiresAuth: true } },
+  { path: '/:catchAll(.*)', redirect: '/' },
 ];
 
 const router = createRouter({
@@ -34,41 +34,37 @@ const router = createRouter({
 });
 
 router.beforeEach(async (to, from, next) => {
-    const userStore = useUserStore();
+  const userStore = useUserStore();
 
-    // 로그인된 사용자가 로그인/회원가입 페이지로 가면 메인으로
-    if (
-        ['/user/login', '/user/signup', '/user/oAuth'].includes(to.path) &&
-        userStore.isFetched.value
-    ) {
-        next('/');
-        return;
-    }
+  // 로그인된 사용자가 로그인/회원가입 페이지로 가면 메인으로
+  if (['/user/login', '/user/signup', '/user/oAuth'].includes(to.path) && userStore.isFetched.value) {
+    next('/');
+    return;
+  }
 
-    // 인증 필요 페이지
-    if (to.meta.requiresAuth && !userStore.isFetched) {
-        try {
-            const isLoggedIn = await userApi.isLoggedIn();
-            console.log(isLoggedIn);
-            if (isLoggedIn) {
-                await userStore.fetchUserInfo();
-                if (!userStore.isFetched) {
-                    next('/user/login');
-                } else {
-                    next();
-                }
-            } else {
-                userStore.reset();
-                next('/user/login');
-            }
-        } catch (e) {
-            userStore.reset();
-            next('/user/login');
+  // 인증 필요 페이지
+  if (to.meta.requiresAuth && !userStore.isFetched) {
+    try {
+      const isLoggedIn = await userApi.isLoggedIn();
+      console.log(isLoggedIn);
+      if (isLoggedIn) {
+        await userStore.fetchUserInfo();
+        if (!userStore.isFetched) {
+          next('/user/login');
+        } else {
+          next();
         }
-    } else {
-        next();
+      } else {
+        userStore.reset();
+        next('/user/login');
+      }
+    } catch (e) {
+      userStore.reset();
+      next('/user/login');
     }
+  } else {
+    next();
+  }
 });
-
 
 export default router;
