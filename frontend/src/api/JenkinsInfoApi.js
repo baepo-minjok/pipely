adc14507c2d1a4664ac3501649bb301f948b67f6
scy <tjcksdud0221@naver.com>
2025-07-25 01:15:29 +0900

    [feat] Jenkins 상세정보 페이지 연결
    Jenkins 상세정보 페이지 연결하고 디자인 수정했습니다.
    Mypage의 정보 목록에서 연결상태를 보이게 추가했습니다.
    
    #180

--- frontend/src/api/JenkinsInfoApi.js
+++ frontend/src/api/JenkinsInfoApi.js
@@ -12,46 +12,44 @@ const instance = axios.create({
 export const jenkinsInfoApi = {
     // Jenkins Info 상세 조회
     getDetail(infoId) {
-        return instance.post("/jenkins/info", { infoId })
+        return instance.post("/jenkins/info", {infoId})
 
-
-            .then((res) => res.data)
+            .then((res) => res)
             .catch((error) => {
-                console.error("getDetail error:", error);
-                throw error.response?.data?.error || error;
+                throw error.response.data.error;
             });
     },
 
     // Jenkins Info 삭제
     delete(infoId) {
         return instance.delete(`/jenkins/info/${infoId}`)
-            .then((res) => res.data)
+            .then((res) => {
+                return true;
+            })
             .catch((error) => {
-                console.error("delete error:", error);
-                throw error.response?.data?.error || error;
+                return false;
             });
     },
 
     // Jenkins Info 수정
     update(payload) {
-
-
-
         return instance.put("/jenkins/info", payload)
-            .then((res) => res.data)
+            .then((res) => {
+                return true;
+            })
             .catch((error) => {
-                console.error("update error:", error);
-                throw error.response?.data?.error || error;
+                return false;
             });
     },
 
     // Jenkins 연결 확인
     verify(infoId) {
-        return instance.post("/jenkins/info/verification", { infoId })
-            .then((res) => res.data)
+        return instance.post("/jenkins/info/verification", {infoId})
+            .then((res) => {
+                return true;
+            })
             .catch((error) => {
-                console.error("verify error:", error);
-                throw error.response?.data?.error || error;
+                return false;
             });
     },
 };
