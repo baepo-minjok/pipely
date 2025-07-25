398a77334ce81c8d35108456669db30514a1a127
scy <tjcksdud0221@naver.com>
2025-07-26 00:36:31 +0900

    [feat] 휴면 유저 재활성화 기능 연결
    휴면 유저가 로그인시 재활성화 여부를 물어보고 재활성화 링크를 이메일로 보내 계정을 다시 사용할 수 있도록 했습니다.
    
    관련 이슈: #226

--- frontend/src/pages/users/Login.vue
+++ frontend/src/pages/users/Login.vue
@@ -3,6 +3,7 @@ import {ref} from "vue";
 import {userApi} from "@/api/UserApi.js";
 import {useRouter} from "vue-router";
 import {useUserStore} from "@/stores/useUserStore.js";
+import {emailApi} from "@/api/EmailApi.js";
 
 const router = useRouter();
 
@@ -22,6 +23,45 @@ const reset = () => {
   emailError.value = false;
   passwordError.value = false;
   errorMessage.value = "";
+}
+
+const reactivation = async (data) => {
+
+  email.value = "";
+  password.value = "";
+  reset();
+
+  const isOk = confirm("탈퇴한 유저입니다.\n 계정을 복구하시겠습니까?");
+
+  if (!isOk) {
+    return;
+  }
+
+  const response = await userApi.reactivation(data);
+  if (response) {
+    alert("계정이 복구되었습니다🎉!\n 다시 로그인해주세요!");
+  } else {
+    alert("오류가 발생했습니다.\n 다시 시도해주세요.");
+  }
+}
+
+const dormant = async (email) => {
+
+  email.value = "";
+  password.value = "";
+  reset();
+
+  const isOk = confirm("휴면 처리된 유저입니다.\n 계정을 복구하시겠습니까?");
+
+  if (!isOk) {
+    return;
+  }
+  const res = await emailApi.sendDormantEmail(email);
+  if (res) {
+    alert("재활성화 이메일이 발송되었습니다!");
+  } else {
+    alert("오류가 발생했습니다.\n 다시 시도해주세요");
+  }
 }
 
 const login = async () => {
@@ -62,25 +102,6 @@ const login = async () => {
     password: password.value,
   };
 
-  const reactivation = async () => {
-
-    const isOk = confirm("탈퇴한 유저입니다.\n 계정을 복구하시겠습니까?");
-
-    if (!isOk) {
-      return;
-    }
-
-    const response = await userApi.reactivation(loginRequest);
-    if (response) {
-      alert("계정이 복구되었습니다🎉!\n 다시 로그인해주세요!");
-    } else {
-      alert("오류가 발생했습니다.\n 다시 시도해주세요.");
-    }
-    email.value = "";
-    password.value = "";
-    reset();
-  }
-
   const response = await userApi.login(loginRequest);
   try {
     if (response.status === 200) { // 로그인 성공;
@@ -92,7 +113,9 @@ const login = async () => {
 
     } else if (response.status === 401) {
       if (response.code === "USER_WITHDRAWN_401") {
-        await reactivation();
+        await reactivation(loginRequest);
+      } else if (response.code === "USER_DORMANT_401") {
+        await dormant(loginRequest.email);
       } else {
         errorMessage.value = response.message;
       }
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import { useUserStore } from '@/stores/useUserStore.js';
-import { userApi } from '@/api/UserApi.js';
+import {createRouter, createWebHistory} from 'vue-router';
+import {useUserStore} from '@/stores/useUserStore.js';
+import {userApi} from '@/api/UserApi.js';
 import Login from '../pages/users/Login.vue';
 import Signup from '../pages/users/Signup.vue';
 import FindPassword from '../pages/users/FindPassword.vue';
@@ -15,6 +15,7 @@ import VerifyEmail from '../pages/users/VerifyEmail.vue';
 import Chat from '../pages/chat/Chat.vue';
 import ResetPassword from '../pages/users/ResetPassword.vue';
 import Withdraw from '../pages/users/Withdraw.vue';
+import ReactivateUser from '../pages/users/ReactivateUser.vue';
 
 const routes = [
     {path: '/', component: Main, name: 'Main'},
@@ -25,6 +26,7 @@ const routes = [
     {path: '/user/find/password', component: FindPassword},
     {path: '/user/reset/password', component: ResetPassword, name: 'ResetPassword'},
     {path: '/user/withdraw', component: Withdraw, name: 'Withdraw'},
+    {path: '/user/reactivate', component: ReactivateUser, name: 'ReactivateUser'},
     {path: '/mypage', component: Mypage, name: 'Mypage', meta: {requiresAuth: true}},
     {path: '/mypage/cicd/create', component: CreateCicdInfo, meta: {requiresAuth: true}},
     {path: '/mypage/cicd/:id', component: CicdInfoDetail, meta: {requiresAuth: true}},
@@ -35,8 +37,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 router.beforeEach(async (to, from, next) => {
