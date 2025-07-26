9dc95c83648611835609aad15b79748499e0e776
scy <tjcksdud0221@naver.com>
2025-07-26 13:55:31 +0900

    [refactor] 필요없는 주석 삭제
    안쓰는 주석을 삭제했습니다.

--- backend/src/main/java/com/example/backend/jenkins/job/model/dto/RequestDto.java
+++ backend/src/main/java/com/example/backend/jenkins/job/model/dto/RequestDto.java
@@ -9,7 +9,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -75,9 +74,6 @@ public class RequestDto {
                 requiredMode = Schema.RequiredMode.NOT_REQUIRED
         )
         private String schedule;
-
-        //@Schema(description = "Job 알림 설정 리스트")
-        //private List<NotificationDto> notificationList;
     }
 
     @Data
