6d3831599e300405ca941262094e72cba5445888
wkdlrn <kkjjgg97@gmail.com>
2025-07-30 01:22:08 +0900

    [refactor] 사용하지 않은 API 호출 제거
    관련이슈: #138, #222, #242

--- frontend/src/api/CalendarApi.js
+++ frontend/src/api/CalendarApi.js
@@ -9,40 +9,6 @@ const instance = axios.create({
 })
 
 export const calendarApi = {
-    // 특정 날짜의 이벤트 조회
-    getEventsByDate(infoId, date) {
-        return instance
-            .get("/calendar/events/by-date", {
-                params: {
-                    infoId: infoId,
-                    date: date,
-                },
-            })
-            .then((res) => {
-                return res
-            })
-            .catch((error) => {
-                return error.response.data.error
-            })
-    },
-
-    // 특정 날짜의 빌드 요약 조회
-    getSummaryByDate(infoId, date) {
-        return instance
-            .get("/calendar/summary/by-date", {
-                params: {
-                    infoId: infoId,
-                    date: date,
-                },
-            })
-            .then((res) => {
-                return res
-            })
-            .catch((error) => {
-                return error.response.data.error
-            })
-    },
-
     // 월별 이벤트 조회
     getEventsByMonth(infoId, year, month) {
         return instance
