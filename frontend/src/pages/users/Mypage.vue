30f4b69fc6340afb433108b1c6901e5bf5e38290
scy <tjcksdud0221@naver.com>
2025-07-31 16:34:27 +0900

    [fix] 버그 수정
    유저 정보를 계속 가져오던 버그를 수정했습니다.

--- frontend/src/pages/users/Mypage.vue
+++ frontend/src/pages/users/Mypage.vue
@@ -2,7 +2,7 @@
 import {useRouter} from 'vue-router';
 import {onMounted, ref} from 'vue';
 import {useUserStore} from "@/stores/useUserStore.js"
-import CheckPasswordModal from "@/pages/users/CheckPasswordModal.vue";
+import CheckPasswordModal from "@/components/modal/CheckPasswordModal.vue";
 
 const userStore = useUserStore();
 const router = useRouter();
@@ -24,9 +24,9 @@ const fetchUser = () => {
 
 const withdraw = () => {
   const isOk = confirm(
-      "정말로 서비스를 탈퇴하시겠습니까?\n\n" +
-      "탈퇴 시 계정 및 모든 데이터가 삭제되며,\n" +
-      "10일 이내에는 다시 로그인하면 복구가 가능합니다."
+    "정말로 서비스를 탈퇴하시겠습니까?\n\n" +
+    "탈퇴 시 계정 및 모든 데이터가 삭제되며,\n" +
+    "10일 이내에는 다시 로그인하면 복구가 가능합니다."
   );
   if (!isOk) {
     return;
@@ -57,7 +57,7 @@ const closePasswordModal = () => {
 onMounted(async () => {
   isLoading.value = true;
   try {
-    if (!userStore.isFetched.value) {
+    if (!userStore.isFetched) {
       fetchUser();
     } else {
       await userStore.fetchUserInfo();
@@ -269,10 +269,10 @@ const goToJobList = (id) => {
 
         <div v-else class="cicd-grid">
           <div
-              v-for="info in infoList"
-              :key="info.id"
-              class="cicd-card"
-              @click="router.push(`/mypage/cicd/${info.id}`)"
+            v-for="info in infoList"
+            :key="info.id"
+            class="cicd-card"
+            @click="router.push(`/mypage/cicd/${info.id}`)"
           >
             <div class="card-header">
               <h4 class="card-title">{{ info.name }}</h4>
@@ -329,9 +329,9 @@ const goToJobList = (id) => {
 
     <!-- 모달 -->
     <CheckPasswordModal
-        v-if="isPasswordModalOpen"
-        :destination="modalTarget"
-        @close="closePasswordModal"
+      v-if="isPasswordModalOpen"
+      :destination="modalTarget"
+      @close="closePasswordModal"
     />
   </div>
 </template>
